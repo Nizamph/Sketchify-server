f03ec0abd5dc237594d6ae7aa5f18edca430a01c
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-12-14 14:56:24 +0530

    removed unwanted code

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const isDev = app.settings.env === 'development';
+const URL = isDev ? 'http://localhost:3000' : '';
+app.use(cors({ origin: URL }));
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: URL });
+
+io.on('connect', (socket) => {
+  socket.on('beginDraw', (arg) => {
+    socket.broadcast.emit('beginDraw', arg);
+  });
+
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg);
+  });
+
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg);
+  });
+});
+
+httpServer.listen(5000);
